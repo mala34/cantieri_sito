ae84cb1e1a453117bb21f81a3094a991c071c3bc
ivan scarciglia <ivan.scarciglia@gmail.com>
2026-04-28 11:50:34 +0200

    added

--- .astro/content.d.ts
+++ .astro/content.d.ts
@@ -120,7 +120,15 @@ declare module 'astro:content' {
 		: any;
 
 	type DataEntryMap = {
-		
+		"blog": Record<string, {
+  id: string;
+  body?: string;
+  collection: "blog";
+  data: any;
+  rendered?: RenderedContent;
+  filePath?: string;
+}>;
+
 	};
 
 	type ExtractLoaderTypes<T> = T extends import('astro/loaders').LiveLoader<
